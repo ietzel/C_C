0952fb21a465e7801b027acb91c3274eeea42191
ietzel <67884277+ietzel@users.noreply.github.com>
2023-07-10 16:34:15 -0800

    Update and rename chord.js to chord.cpp

--- chord.cpp
+++ chord.cpp
@@ -4,21 +4,24 @@ tonic = [1, 4, 5, 10];
 dominant = [2, 8, 11, 12];
 predominant = [3, 6, 7, 9];
 
+symbols []
+
+//params t, d, p, are the weights of tonic, dominant, predominant, respectively
 function randomChord(t, d, p) {
-	let result = Math.random(); 
+	let result = Math.random()*(t+d+p); 
 	let index = Math.floor(Math.random()*4);
 	if(index > 3) {
 		index = 3;
 	}
 	switch(result) {
-		case t:
+		case result < t:
 			tonic[index];
 			break;
-		case d:
-			tonic[index];
+		case result < t+d:
+			dominant[index];
 			break;
-		case s:
-			tonic[index];
+		default:
+			predominant[index];
 			break;
 	}
 }
